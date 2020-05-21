6be4e1d7d6391b0ca22b1bb2ebe4d60cacec92d4
santiquiroz <squirozu@unal.edu.co>
2020-05-21 13:09:37 -0500

    basic services

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -15,6 +15,8 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import {DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +34,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule
     
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
