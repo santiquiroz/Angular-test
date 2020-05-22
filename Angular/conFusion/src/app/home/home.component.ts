a2ccbd8358fd00a7db7ce37f1170fe7f69920636
santiquiroz <squirozu@unal.edu.co>
2020-05-22 12:12:31 -0500

    assgnment 2 correction

--- Angular/conFusion/src/app/home/home.component.ts
+++ Angular/conFusion/src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../shared/Promotion';
 import { PromotionService } from '../services/Promotion.service';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/Leader.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,13 +15,16 @@ export class HomeComponent implements OnInit {
 
   dish: Dish;
   promotion: Promotion;
+  leader: Leader;
 
   constructor(private DishService: DishService,
-    private promotionService: PromotionService) { }
+    private promotionService: PromotionService,
+    private leaderService: LeaderService) { }
 
   ngOnInit() {
     this.dish = this.DishService.getFeaturedDish();
     this.promotion = this.promotionService.getFeaturedPromotion();
+    this.leader = this.leaderService.getFeaturedLeader();
   }
 
 }
